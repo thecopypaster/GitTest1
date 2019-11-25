18d52e65f6791b1027c3fc07672c10c995a24ead
thecopypaster <bhanumurthy24@gmail.com>
2019-11-25 16:33:46 -0600

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Code was edited in Github
+            // Code was edited again in Github
         }
     }
 }
